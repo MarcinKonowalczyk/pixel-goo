4ea280f42c77865137446a23879f582a0ecd503c
Marcin <marcin.konow@lczyk.xyz>
2021-06-04 12:51:09 +0100

    old main #2

--- main.cpp
+++ main.cpp
@@ -1,89 +1,198 @@
-#include "mingl.hpp"
 #include <iostream>
 
-#include "GLFW/glfw3.h"
+#include <glad/glad.h> // OpenGL functions
+#include <GLFW/glfw3.h>
 
-void drawLine(int x0, int y0, int x1, int y1, const MinGLColor& color, const MinGL& minGL) {
-    bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
-    }
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    int derror = std::abs(dy) * 2;
-    int error = 0;
-    int y = y0;
-    for (int x = x0; x < x1; ++x) {
-        if (steep) {
-            minGL.putPixel(y, x);
-        }
-        else {
-            minGL.putPixel(x, y);
-        }
-        error += derror;
-        if (error > dx) {
-            y += (y1 > y0 ? 1 : -1);
-            error -= dx * 2;
-        }
-    }
+// float vertices[] = {
+//     -1.0f, -1.0f,
+//     1.0f, -1.0f,
+//     1.0f, 1.0f,
+//     -1.0f, -1.0f,
+//     1.0f, 1.0f,
+//     -1.0f, 1.0f,
+// };
+
+// unsigned VAO;
+// glGenVertexArrays(1, &VAO);
+// glBindVertexArray(VAO);
+
+// unsigned int VBO;
+// glGenBuffers(1, &VBO);
+// glBindBuffer(GL_ARRAY_BUFFER, VBO);
+// glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+// glVertexAttribPointer (
+//     0,                    // Location = 0
+//     2,                    // Size of vertex attribute
+//     GL_FLOAT,            // Type of the data
+//     GL_FALSE,            // Normalize data?
+//     2 * sizeof(float),    // Stride
+//     (void*)0            // Offset
+// );
+// glEnableVertexAttribArray(0/*Location*/);
+// glBindVertexArray(0);
+
+// GLuint vertexShader;
+// vertexShader = glCreateShader(GL_VERTEX_SHADER);
+// glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+// glCompileShader(vertexShader);
+
+// int success;
+// char infoLog[512];
+// glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+// if (!success) {
+//     glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+//     fprintf(stderr, infoLog);
+//     return false;
+// }
+
+// GLuint fragmentShader;
+// fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+// glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+// glCompileShader(fragmentShader);
+
+
+// glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+// if (!success) {
+//     glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+//     fprintf(stderr, infoLog);
+//     return false;
+// }
+
+// m_shaderProgram = glCreateProgram();
+// glAttachShader(m_shaderProgram, vertexShader);
+// glAttachShader(m_shaderProgram, fragmentShader);
+// glLinkProgram(m_shaderProgram);
+// glGetProgramiv(m_shaderProgram, GL_LINK_STATUS, &success);
+// if (!success) {
+//     glGetProgramInfoLog(m_shaderProgram, 512, nullptr, infoLog);
+//     fprintf(stderr, infoLog);
+//     return false;
+// }
+// glDeleteShader(vertexShader);
+// glDeleteShader(fragmentShader);
+
+// glUseProgram(m_shaderProgram);
+// glBindVertexArray(VAO);
+
+// glEnable(GL_BLEND);
+// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+// return true;
+
+GLFWwindow* window;
+int width = 800;
+int height = 600;
+const char* title = "Goolets";
+bool fullscreen = false;
+
+struct particle {
+    float x;
+    float y;
+};
+const int P = 1000; // Number of particles
+particle x[P];
+
+void window_setup();
+
+void particles_setup() {
+
 }
 
-const int width = 800;
-const int height = 600;
-
-
 int main() {
-    // Window sizes
-    MinGL minGL;
-    if (!minGL.init(width, height, "MinGL")) {
-        return -1;
-    }
+    window_setup();
 
-    int i = 0;
-    while (!minGL.windowShouldClose()) {
+    while (!glfwWindowShouldClose(window)) {
+
         float start = glfwGetTime();
-        minGL.pollEvents();
-        minGL.processInput();
 
-        // Draw a green cube
-        const MinGLColor green = { 0.0f, 1.0f, 0.0f, 1.0f };
-        int c0X = 300 + i;
-        int c0Y = 250 + i;
-        int c1X = c0X;
-        int c1Y = c0Y + 100;
-        drawLine(c0X, c0Y, c1X, c1Y, green, minGL);
-        int c2X = c0X + 100;
-        int c2Y = c0Y + 100;
-        drawLine(c1X, c1Y, c2X, c2Y, green, minGL);
-        int c3X = c0X + 100;
-        int c3Y = c0Y;
-        drawLine(c2X, c2Y, c3X, c3Y, green, minGL);
-        drawLine(c3X, c3Y, c0X, c0Y, green, minGL);
-        int c4X = c1X + 50;
-        int c4Y = c1Y + 50;
-        drawLine(c1X, c1Y, c4X, c4Y, green, minGL);
-        int c5X = c4X + 100;
-        int c5Y = c4Y;
-        drawLine(c4X, c4Y, c5X, c5Y, green, minGL);
-        drawLine(c2X, c2Y, c5X, c5Y, green, minGL);
-        int c6X = c3X + 50;
-        int c6Y = c3Y + 50;
-        drawLine(c3X, c3Y, c6X, c6Y, green, minGL);
-        drawLine(c5X, c5Y, c6X, c6Y, green, minGL);
+        // int key = glfwGetKey(window, GLFW_KEY_ESCAPE);
+        // if (key) {
+            // std::cout << key << std::endl;
+        // }
+        // if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            // glfwSetWindowShouldClose(window, true);
+        // }
 
-        minGL.flush(0.12f, 0.12f, 0.12f, 0.10f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // float white[] = {1.0f,1.0f,1.0f};
+        // glDrawPixels(10,10,GL_RGB,GL_FLOAT,white);
+        glBegin(GL_TRIANGLES);
+            glColor3f(0.067f, 0.455f, 0.729f); glVertex2f( 0.0f, 0.5f);
+            glColor3f(0.843f, 0.329f, 0.149f); glVertex2f(-0.5f,-0.5f);
+            glColor3f(0.925f, 0.690f, 0.208f); glVertex2f( 0.5f,-0.5f);
+        glEnd();
+        // glBegin(GL_TRIANGLES);
+        // glVertex2f( -0.5f, -0.5f);
+        // glVertex2f( 0.0f,   0.5f);
+        // glVertex2f( 0.5f,  -0.5f);
+        // glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
         float end = glfwGetTime();
         std::cout << (end-start)*1000 << std::endl;
-        i++;
     }
 
     // Clean up
-    minGL.shutdown();
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
+}
+
+void window_setup() {
+    // Setup window
+    glfwSetErrorCallback([](int error, const char* description) {
+        fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+    });
+
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+
+    // GL 2.1
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    GLFWmonitor* primaryMonitor;
+    if (fullscreen) {
+        primaryMonitor = glfwGetPrimaryMonitor();
+        const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+        width = mode->width;
+        height = mode->height;
+    } else {
+        primaryMonitor = nullptr;
+    }
+
+    std::cout << width << " " << height << std::endl;
+    
+    window = glfwCreateWindow(width, height, title, primaryMonitor, nullptr);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int framebufferWidth, framebufferHeight;
+    glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+    std::cout << width << "->" << framebufferWidth << std::endl;
+    std::cout << height << "->" << framebufferHeight << std::endl;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+    
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    });
+
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        if (action == GLFW_PRESS) {
+            std::cout << key << std::endl;
+            if (key == GLFW_KEY_ESCAPE) {glfwSetWindowShouldClose(window, true);}
+        }
+    });
+
+    // GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+    }
 }
